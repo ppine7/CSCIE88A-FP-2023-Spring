20904b1c3205ad1e5f3f75f7ff2c74eb475c522a
Marina Popova <ppine7@users.noreply.github.com>
2023-02-07 09:20:23 -0500

    week3 Java code Problem1

--- fp-java/src/main/java/cscie88a/week3/AdoptionService.java
+++ fp-java/src/main/java/cscie88a/week3/AdoptionService.java
@@ -2,19 +2,14 @@ package cscie88a.week3;
 
 public class AdoptionService {
 
-    public boolean tryToAdoptNoArg(
-            IAdoptable someoneToAdopt, boolean adoptionFeePaid) {
-        if (someoneToAdopt.readyForAdoptionNoArg() && adoptionFeePaid) {
-            return true;
-        } else
-            return false;
+    public boolean tryToAdoptNoArg(IAdoptable someoneToAdopt, boolean adoptionFeePaid) {
+        // TODO implement this
+        return true;
     }
 
     public boolean tryToAdoptOneArg(IAdoptable2 someoneToAdopt, boolean healthCheckDone, boolean adoptionFeePaid) {
-        if (someoneToAdopt.readyForAdoptionOneArg(healthCheckDone) && adoptionFeePaid) {
-            return true;
-        } else
-            return false;
+        // TODO implement this
+        return true;
     }
 
 }
