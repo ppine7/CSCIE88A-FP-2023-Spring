b39d0622b98688f1f39026ec0a66236cc9a7a0b6
Marina Popova <ppine7@users.noreply.github.com>
2023-02-07 21:50:13 -0500

    added comments

--- fp-python/week3/demo_week3.py
+++ fp-python/week3/demo_week3.py
@@ -1,4 +1,4 @@
-
+# function that takes another function (with no arguments) as a parameter
 def train_for_tricks_no_arg(do_trick_no_arg_fn):
     if do_trick_no_arg_fn():
         print("I'm doing the trick!")
@@ -8,6 +8,9 @@ def train_for_tricks_no_arg(do_trick_no_arg_fn):
         return False
 
 
+# function that takes another function (with ONE arguments) as a parameter
+# do_trick_one_arg_fn: input parameter function
+# trick_name: argument for the do_trick_one_arg_fn function
 def train_for_tricks_one_arg(do_trick_one_arg_fn, trick_name):
     if do_trick_one_arg_fn(trick_name):
         print("I'm doing the trick: {}!".format(trick_name))
